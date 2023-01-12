812dbcad6545cda89c9f6e774b5ccb29346cff80
vluespider <vluespider@gmail.com>
2023-01-11 21:05:05 -0500

    completed

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Card from './components/Card'
+import { useFetchRepositories } from './hooks/useRepos'
+import { useFavoriteReposStore } from './store/favoriteRepos'
+
+function App() {
+  const { data, isLoading } = useFetchRepositories('fazt')
+  const { favoriteReposIds } = useFavoriteReposStore()
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <div>
+      {data?.map(repository => (
+        <Card
+          key={repository.id}
+          repository={repository}
+          isFavorite={favoriteReposIds.includes(repository.id)}
+        />
+      ))}
+    </div>
+  )
+}
+export default App
